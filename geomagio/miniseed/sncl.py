34b4f48ef0a21c092890df662c8d6c2c05b21f16
Jeremy Fee <jmfee@usgs.gov>
2019-06-07 17:05:14 -0600

    Initial sncl implementation and tests

---
+++ geomagio/miniseed/sncl.py
@@ -0,0 +1,86 @@
+"""MiniSEED utilities
+"""
+
+SEED_CHANNEL_PREFIX = {
+  'B': 'tenhertz',
+  'L': 'second',
+  'U': 'minute',
+  'R': 'hour',
+  'P': 'day'
+}
+SEED_CHANNEL_TYPE = {
+  'E': 'Volt',
+  'Y': 'Bin'
+}
+SEED_LOCATION_PREFIX = {
+  'R': 'variation',
+  'A': 'adjusted',
+  'Q': 'quasi-definitive',
+  'D': 'definitive'
+}
+SEED_LOCATION_SUFFIX = {
+  'D': 'Dist',
+  'Q': 'SQ',
+  'V': 'SV'
+}
+
+ELEMENT_CHANNEL_TYPE    = { v:k for k,v in SEED_CHANNEL_TYPE.items() }
+ELEMENT_LOCATION_SUFFIX = { v:k for k,v in SEED_LOCATION_SUFFIX.items() }
+INTERVAL_CHANNEL_PREFIX = { v:k for k,v in SEED_CHANNEL_PREFIX.items() }
+TYPE_LOCATION_PREFIX    = { v:k for k,v in SEED_LOCATION_PREFIX.items() }
+
+
+def decode_sncl(network, station, channel, location):
+  return {
+    'network': network,
+    'observatory': station,
+    'element': get_element(network, station, channel, location),
+    'data_interval': get_data_interval(network, station, channel, location),
+    'data_type': get_data_type(network, station, channel, location)
+  }
+
+def encode_sncl(network, observatory, element, data_interval, data_type):
+  return {
+    'network': network,
+    'station': observatory,
+    'channel': get_sncl_channel(network, observatory, element, data_interval, data_type),
+    'location': get_sncl_location(network, observatory, element, data_interval, data_type)
+  }
+
+def get_data_interval(network, station, channel, location):
+  return SEED_CHANNEL_PREFIX[channel[0]]
+
+def get_data_type(network, station, channel, location):
+  return SEED_LOCATION_PREFIX[location[0]]
+
+def get_element(network, station, channel, location):
+  element_prefix = channel[2]
+  element_suffix = ''
+  channel_type = channel[1]
+  if channel_type in SEED_CHANNEL_TYPE:
+    element_suffix += '_' + SEED_CHANNEL_TYPE[channel_type]
+  location_suffix = location[1]
+  if location_suffix in SEED_LOCATION_SUFFIX:
+    element_suffix += '_' + SEED_LOCATION_SUFFIX[location_suffix]
+  return element_prefix + element_suffix
+
+def get_sncl_channel(network, observatory, element, data_interval, data_type):
+  channel_prefix = INTERVAL_CHANNEL_PREFIX[data_interval]
+  element_parts = element.split('_')
+  channel_suffix = element_parts[0]
+  channel_middle = 'F'
+  if len(element_parts) > 1:
+    rest = element_parts[1]
+    if rest in ELEMENT_CHANNEL_TYPE:
+      channel_middle = ELEMENT_CHANNEL_TYPE[rest]
+  return channel_prefix + channel_middle + channel_suffix
+
+def get_sncl_location(network, observatory, element, data_interval, data_type):
+  location_prefix = TYPE_LOCATION_PREFIX[data_type]
+  location_suffix = '0'
+  element_parts = element.split('_')
+  if len(element_parts) > 1:
+    rest = element_parts[1]
+    if rest in ELEMENT_LOCATION_SUFFIX:
+      location_suffix = ELEMENT_LOCATION_SUFFIX[rest]
+  return location_prefix + location_suffix
---
+++ test/miniseed_test/sncl_test.py
@@ -0,0 +1,72 @@
+"""Tests for the IMFV283 Parser class."""
+
+from nose.tools import assert_equals
+from obspy import UTCDateTime
+
+from geomagio.miniseed import sncl
+
+TESTS = [
+  [
+    {
+      'network': 'NT',
+      'station': 'TEST',
+      'channel': 'BEU',
+      'location': 'R0'
+    },
+    {
+      'network': 'NT',
+      'observatory': 'TEST',
+      'element': 'U_Volt',
+      'data_type': 'variation',
+      'data_interval': 'tenhertz'
+    }
+  ],
+
+  [
+    {
+      'network': 'NT',
+      'station': 'TEST',
+      'channel': 'LFH',
+      'location': 'RD'
+    },
+    {
+      'network': 'NT',
+      'observatory': 'TEST',
+      'element': 'H_Dist',
+      'data_type': 'variation',
+      'data_interval': 'second'
+    }
+  ],
+
+  [
+    {
+      'network': 'NT',
+      'station': 'TEST',
+      'channel': 'UFZ',
+      'location': 'A0'
+    },
+    {
+      'network': 'NT',
+      'observatory': 'TEST',
+      'element': 'Z',
+      'data_type': 'adjusted',
+      'data_interval': 'minute'
+    }
+  ]
+
+
+]
+
+def test_decode_sncl():
+    """miniseed_test.sncl_test.test_decode_sncl()
+    """
+    for test in TESTS:
+      actual = sncl.decode_sncl(**test[0])
+      assert_equals(actual, test[1])
+
+def test_encode_sncl():
+    """miniseed_test.sncl_test.test_encode_sncl()
+    """
+    for test in TESTS:
+      actual = sncl.encode_sncl(**test[1])
+      assert_equals(actual, test[0])
